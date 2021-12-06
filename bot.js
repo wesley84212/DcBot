439631bed69297139a3841403cd0e79d97244692
ahri's mac <ahri@ahrisdeMacBook-Air.local>
2021-12-06 20:51:23 +0800

    Change: test

--- bot.js
+++ bot.js
@@ -1,51 +1,42 @@
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
-const { command, imageFile, privateMessage } = require("./data.json");
+const {command, imageFile, privateMessage} = require("./data.json");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('interactionCreate', async interaction => {
-	const userReactions = message.reactions.cache.filter(reaction => reaction.users.cache.has(userId));
-		console.log(userReactions)
-	try {
-		for (const reaction of userReactions.values()) {
-			await reaction.users.remove(userId);
+			if (!interaction.isCommand()) return;
+			const { commandName } = interaction;
+			if (commandName === 'ping') {
+				await interaction.reply('Pong!');
+			} else if (commandName === 'server') {
+				await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+			} else if (commandName === command) {
+				let inputStr = interaction.options.getString('input')
+				inputStr = inputStr.toUpperCase();
+				const member = interaction.member;
+				const role = interaction.guild.roles.cache.find((r)=>{
+					return r.name.indexOf(inputStr) > -1
+				});
+				if(!!role === false){
+					return;
+				}
+				try{
+					member.roles.add(role);
+					console.log(`名字: ${interaction.user.tag}\n已加入: ${role.name}`)
+					await interaction.user.send({files:[imageFile]})
+					await interaction.user.send(privateMessage)
+					await interaction.user.send("「請注意未加上特別存取碼的儲藏室內容為錯誤資訊，請勿直接存取，否則取消資格」");
+					await interaction.user.send({files:["./messageImg.jpg"]})
+					await interaction.reply(`試煉者: ${interaction.user.tag}已被傳送`)
+				 }catch(e){
+					console.log(e)
+					console.error(e)
+				 }		
 		}
-	} catch (error) {
-		console.error('Failed to remove reactions.');
-	}
-	if (!interaction.isCommand()) return;
-	const { commandName } = interaction;
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === command) {
-		let inputStr = interaction.options.getString('input')
-		inputStr = inputStr.toUpperCase();
-		const member = interaction.member;
-		const role = interaction.guild.roles.cache.find((r) => {
-			return r.name.indexOf(inputStr) > -1
-		});
-		if (!!role === false) {
-			return;
-		}
-		try {
-			member.roles.add(role);
-			console.log(`名字: ${interaction.user.tag}\n已加入: ${role.name}`)
-			await interaction.user.send({ files: [imageFile] })
-			await interaction.user.send(privateMessage)
-			await interaction.user.send("「請注意未加上特別存取碼的儲藏室內容為錯誤資訊，請勿直接存取，否則取消資格」");
-			await interaction.user.send({ files: ["./messageImg.jpg"] })
-			await interaction.reply(`試煉者: ${interaction.user.tag}已被傳送`)
-		} catch (e) {
-			console.log(e)
-			console.error(e)
-		}
-	}
-});
+	});
 client.on("guildMemberAdd", (member) => {
 	const guild = member.guild;
 	console.log(guild.channels.cache)
